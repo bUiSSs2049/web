f6f7b4f5cc3f74d75c72360a810044e2f4a54e3a
aissa <89082595+bUiSSs2049@users.noreply.github.com>
2023-04-23 14:57:28 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     'connection':{
      'host':process.env.HOST,
      'user':process.env.USER,
-     'password':process.env.PQSS
+     'password':process.env.PASS
     },
     'database':process.env.DB,
     'user_table':'users'
